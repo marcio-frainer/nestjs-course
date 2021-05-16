ce2174233d077e0089826f78f178cc57b1d0a271
marcio.frainer <marcio.frainer@softplan.com.br>
2021-05-16 19:44:44 -0300

    refactoring player service and controller

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,34 +1,44 @@
-import { Body, Controller, Get, Param, Post, Logger, Query, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Logger, Query, Delete, UsePipes, ValidationPipe, Put } from '@nestjs/common';
 import { PlayerDto } from './dtos/Player.dto'
 import { Player } from './interfaces/Player.interface';
 import { PlayersService } from './Players.service';
 import { PlayersValidationParamsPipe } from './pipes/players-validation-params.pipe';
 
-@Controller('api/v1/Playeres')
+@Controller('api/v1/players')
 export class PlayersController {
 
     constructor(private readonly PlayersService: PlayersService) { };
 
-    // private readonly logger = new Logger(PlayeresService.name);
+    private readonly logger = new Logger(PlayersController.name);
 
     @Post()
     @UsePipes(ValidationPipe)
-    async create(@Body() Player: PlayerDto) {
-        await this.PlayersService.create(Player);
+    async create(@Body() player: PlayerDto) {
+        await this.PlayersService.create(player);
+    }
+
+    @Put(':_id')
+    @UsePipes(ValidationPipe)
+    async update(
+        @Body() player: PlayerDto,
+        @Param('_id', PlayersValidationParamsPipe) _id: string): Promise<void> {
+        await this.PlayersService.update(_id, player);
     }
 
     @Get()
-    async findOne(@Query('email') email: string): Promise<Player[] | Player> {
-        if (email) {
-            return this.PlayersService.findOne(email);
-        }
-
+    async findAll(): Promise<Player[]> {
         return this.PlayersService.findAll();
     }
 
-    @Delete()
-    async delete(@Query('email', PlayersValidationParamsPipe) email: string): Promise<void> {
-        return await this.PlayersService.delete(email);
+    @Get('/:_id')
+    async findOne(@Param('_id', PlayersValidationParamsPipe) _id: string): Promise<Player> {
+        this.logger.log(`findOne: ${_id}`);
+        return this.PlayersService.findOne(_id);
+    }
+
+    @Delete('/:_id')
+    async delete(@Param('_id', PlayersValidationParamsPipe) _id: string): Promise<void> {
+        return await this.PlayersService.delete(_id);
     }
 
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Player } from './interfaces/player.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -12,19 +12,36 @@ export class PlayersService {
     private readonly logger = new Logger(PlayersService.name);
 
     public async create(player: PlayerDto): Promise<void> {
+        const { email } = player;
+        const findedPlayer = this.findEmail(email);
+        if (findedPlayer) {
+            throw new BadRequestException(`Player with email: ${email} already exists!`);
+        }
         this.createPlayer(player);
+    }
+
+    public async update(_id: string, player: PlayerDto): Promise<void> {
+        const findedPlayer = this.findOne(_id);
+        if (!findedPlayer) {
+            throw new NotFoundException(`Player ${_id} not found!`);
+        }
+        this.playerModel.findOneAndUpdate({ _id }, { $set: player }).exec();
     }
 
     public async findAll(): Promise<Player[]> {
         return this.playerModel.find().exec();
     }
 
-    public async findOne(email: string): Promise<Player> {
+    public async findEmail(email: string): Promise<Player> {
         return this.playerModel.findOne({ email }).exec();
     }
 
-    public async delete(email: string): Promise<void> {
-        this.playerModel.deleteOne({ email }).exec();
+    public async findOne(_id: string): Promise<Player> {
+        return this.playerModel.findOne({ _id }).exec();
+    }
+
+    public async delete(_id: string): Promise<void> {
+        this.playerModel.deleteOne({ _id }).exec();
     }
 
     private async createPlayer(playerDto: PlayerDto): Promise<Player> {
